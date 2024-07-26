5759c3205c6928e901bdc8fbfd57950f265ba1df
dkoklev <danikoklef@gmail.com>
2024-07-26 14:57:31 +0300

    Working on pipeline

--- automation/startjanssenmonolithdemo.sh
+++ automation/startjanssenmonolithdemo.sh
@@ -159,7 +159,7 @@ docker exec -w /tmp/jans/jans-keycloak-integration docker-jans-monolith-jans-1 m
 docker exec -w /tmp/jans/jans-auth-server docker-jans-monolith-jans-1 mvn -Dcfg="$JANS_FQDN" -Dmaven.test.skip=false test
 echo -e "copying reports.. \n"
 docker exec docker-jans-monolith-jans-1 ls -lha
-docker exec docker-jans-monolith-jans-1 ls -lha /tmp/
+docker exec docker-jans-monolith-jans-1 ls -lha /tmp/jans/
 docker cp docker-jans-monolith-jans-1:/tmp/jans/jans-auth-server/client/target/surefire-reports/testng-results.xml /tmp/reports/$JANS_PERSISTENCE-jans-auth-client-testng-results.xml
 docker cp docker-jans-monolith-jans-1:/tmp/jans/jans-auth-server/agama/model/target/surefire-reports/testng-results.xml /tmp/reports/$JANS_PERSISTENCE-jans-auth-agama-model-testng-results.xml
 docker cp docker-jans-monolith-jans-1:/tmp/jans/jans-auth-server/test-model/target/surefire-reports/testng-results.xml /tmp/reports/$JANS_PERSISTENCE-jans-auth-test-model-testng-results.xml
